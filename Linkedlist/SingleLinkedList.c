4bbbaca607ae1b2c291fddb0ff1edc3211fdeb66
Maduki-tech <d.schlueter1011@gmail.com>
2023-11-29 19:58:40 +0100

    basic linked list in c

--- Linkedlist/SingleLinkedList.c
+++ Linkedlist/SingleLinkedList.c
@@ -1,9 +1,46 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char *argv[]) {
+typedef struct Node Node;
 
-    printf("hello World\n");
+struct Node {
+    int data;
+    struct Node *next;
+};
+
+Node *createNode(int data) {
+    Node *node = (Node *)malloc(sizeof(Node));
+    node->data = data;
+    node->next = NULL;
+    return node;
+}
+
+void insertNode(Node **head, int data) {
+    Node *node = createNode(data);
+    if (*head == NULL) {
+        *head = node;
+    } else {
+        Node *temp = *head;
+        while (temp->next != NULL) {
+            temp = temp->next;
+        }
+        temp->next = node;
+    }
+}
+
+void printList(Node *head) {
+    while (head != NULL) {
+        printf("%d ", head->data);
+        head = head->next;
+    }
+}
+
+int main(int argc, char *argv[]) {
+    Node *head = NULL;
+    insertNode(&head, 1);
+    insertNode(&head, 2);
+    insertNode(&head, 3);
+    printList(head);
 
     return EXIT_SUCCESS;
 }
